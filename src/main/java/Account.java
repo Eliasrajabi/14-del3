e28b41cceeadcd45ea65497c4a326ce10f5d60a3
Mathilde Elia <mathildeselia@gmail.com>
2021-11-09 11:32:42 +0100

    Konto-klassen fra CDIO2 er blevet tilføjet. Den er blevet opdateret så der ikke sker noget med spillerens pengetotal hvis ikke de har penge nok til at købe en grund.

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,7 +1,7 @@
 public class Account {
     int moneyTotal;
 
-    public boolean adjustMoney(int money){
+    public boolean isBoothBought(int money){
         int CheckBalance = moneyTotal + money;
 
         if(CheckBalance<0){
@@ -11,5 +11,16 @@ public class Account {
             moneyTotal += money;
             return true;
         }
+    }
+    public int payFine(int money){
+        int CheckBalance = moneyTotal + money;
+
+        if(CheckBalance<0){
+            moneyTotal = 0;
+        }
+        else {
+            moneyTotal += money;
+        }
+        return moneyTotal;
     }
 }
