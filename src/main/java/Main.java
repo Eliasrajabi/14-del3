6e984ae83a0fbc35bae8560c3dda172c4ca311a9
malte <maltebjoerklund@gmail.com>
2021-11-12 19:59:26 +0100

    Læsning af tekstfil

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-
-
+        System.out.println(Text.Descriptions(4));
 
     }
 
--- src/main/java/Text.java
+++ src/main/java/Text.java
@@ -1,5 +1,38 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Text {
 
+    public static String Descriptions(int squareNr) throws IOException {
+
+        File fil = new File("C:\\Users\\Malte\\IdeaProjects\\CDIO-3\\TestAfTekst");
+
+        Scanner scan = new Scanner(new FileReader(fil.toString()));
+
+        String squareDesc = "";
+
+        while(scan.hasNextLine()){
+            scan.nextLine();
+
+            if(scan.hasNext(Integer.toString(squareNr))){
+                scan.nextLine();
+
+                while(scan.hasNextLine()){
+
+                    squareDesc = String.format("%s%s\n", squareDesc, scan.nextLine());
+
+                    if(scan.hasNext(Integer.toString(squareNr+1))){
+                        break;
+                    }
+                }
+
+                break;
+            }
+
+        }
+
+        return squareDesc;
+    }
 
 
 }
